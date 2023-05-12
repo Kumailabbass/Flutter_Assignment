41f15db97f923f078723b38b662e36cf5292a81a
Kumailabbass <kumailabbass2004@gmail.com>
2023-05-13 03:52:50 +0500

    Quote app buttons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,9 @@ import 'package:flutter_quote_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    MyApp() async {
+      await tester.pumpWidget(MyApp());
+    }
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
